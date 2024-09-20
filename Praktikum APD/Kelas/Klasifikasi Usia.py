1ed9e59057aac752602feb0c3059ca3b9c8b9024
XZNVWS <devonfalen100@gmail.com>
2024-09-20 14:59:58 +0800

    Add files via upload

---
+++ Praktikum APD/Kelas/Klasifikasi Usia.py
@@ -0,0 +1,11 @@
+umur = int(input("Masukkan umur Anda :"))
+           
+if umur <= 10 : print("Umur Anda termasuk kategori anak-anak")
+
+elif umur <= 18 : print("Umur Anda termasuk kategori remaja")
+
+elif umur <= 35 : print("Umur Anda termasuk kategori dewasa")
+
+elif umur <= 65 : print("Umur Anda termasuk kategori paruh baya")
+
+else : print("Umur Anda termasuk kategori Tua")
