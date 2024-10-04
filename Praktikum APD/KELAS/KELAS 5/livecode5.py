5b59e8b33e957e6432ab3b1a0015031f254b4701
MoonYeon <devonfalen100@gmail.com>
2024-10-04 15:20:28 +0800

    Add files via upload

---
+++ Praktikum APD/KELAS/KELAS 5/livecode5.py
@@ -0,0 +1,65 @@
+# data= [1,2,3,4,5]
+# data_2 = list((1,2,3,4,5))
+
+# print (data [5] [0])
+# # print(type(data_2))
+
+# data = ["apel","durian","jeruk"]
+# print(data)
+# print()
+# data.extend("semangka",  "mangga")
+
+# data.append("semangka")
+# data.append(True)
+# print(data)
+# data.insert(0,"rambutan")
+# print(data)
+
+# data = ["apel","durian","jeruk"]
+# print(data)
+# print()
+# # del data[2]
+# # # data[0:2]  = ["mangga","semangka"]
+# # nilai_hapus = data.pop (2)
+# data.remove("apel")
+# print(data)
+
+# data_mahasiswa =[
+    # ["060", "infu"["apd","kaska"]],
+    
+    # ["070", "infu"]
+    # ]
+# data_mahasiswa = [
+#     ["admin","admin111","admin131"]
+#     ["faf","fufu","fafu"]
+# ]
+
+# print(data_mahasiswa[0][2][0])
+
+# buah = ["apel","durian","jeruk"]
+# index
+# for data in buah:
+#     print(data)
+
+
+# for index in range(len(buah)):
+#     print
+#     print(buah[index])
+
+# for index, item in enumerate(buah):
+#     print(f"data ke {index+1}")
+#     (f" : {item}")
+
+# import os
+# data_mahasiswa= ["dep",  "depon", "devon""] 
+# print(data_mahasiswa)
+# print()
+# data_mahasiswa[0:2]  = ["devon","falen"]
+# print(data_mahasiswa)
+
+
+# print(data_mahasiswa)
+# print("menu")
+# print("=" *10)
+
+
