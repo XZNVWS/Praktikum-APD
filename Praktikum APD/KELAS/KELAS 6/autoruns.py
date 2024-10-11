298286fdbfd8b838d613d9cd5d90436f433406d6
MoonYeon <devonfalen100@gmail.com>
2024-10-11 14:55:09 +0800

    Add files via upload

---
+++ Praktikum APD/KELAS/KELAS 6/autoruns.py
@@ -0,0 +1,67 @@
+#DICTIONARY
+
+# # print(data_mhs["nama"])
+
+# print(data_mhs.get("tes"),'hah?')
+
+# for data in data_mhs:
+#     print(data)
+
+# for key_data, value_data in data_mhs.items():
+#     print(f"key:, {key_data},value:,{value_data} ")
+
+# data_mhs['alamat']='Samarinda'
+
+# data_mhs.update({"alamat":"samarinda"})
+# data_mhs.update({"alamat":"semarang"})
+
+# data_mhs = {
+# "nama":"ucup",
+# 'NIM': 001,
+# }
+
+# del data_mhs['NIM']
+
+# hapus = data_mhs.pop('NIM')
+# print(data_mhs)
+# print(hapus)
+
+# data_mhs['no'] = hapus
+# print(data_mhs)
+
+# print(data_mhs.clear(), "clear")
+
+# key = "apel", "jeruk", "mangga","semangka","anggur"
+# value = 1
+# buah = dict.fromkeys(key, value)
+# print(buah)
+# Nilai = {
+# "Matematika" : 90,
+# "B. Indonesia" : 80,
+# "Biologi" : 80,
+# "Kimia" : 20
+# }
+# print(Nilai)
+# print("Nilai : ", Nilai.setdefault("Kimia", 70))
+# print("")
+# print(Nilai)
+
+# datamhs={
+# "nama":"ucup",
+# "nim": 1,
+# "matkul":["APD",'KALKULUS',"JARKOM"],
+# "Dosen" : {"nama":"Pak Awang",
+#            "matkul":"APD"}}
+
+# print(datamhs["Dosen"]["nama"])
+
+# datamhs=[{
+# "nama":"ucup",
+# "role":"user"
+# },
+# {
+# "nama":"ucup",
+# "role":"admin"
+# }
+
+# print(datamhs[0]["nama"])
