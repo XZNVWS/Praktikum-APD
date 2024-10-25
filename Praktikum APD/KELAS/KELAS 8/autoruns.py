6a9b6e85b5a95685422ea54a1a57887efe371caf
MoonYeon <devonfalen100@gmail.com>
2024-10-25 15:25:35 +0800

    Add files via upload

---
+++ Praktikum APD/KELAS/KELAS 8/autoruns.py
@@ -0,0 +1,74 @@
+
+# angka = int(input("Masukkan Angka: "))
+# print(angka)
+
+
+# try: 
+#    angka = int(input("Masukkan Angka: "))
+# except ValueError:
+#    print("yang anda inputkan bukan angka!") 
+
+
+# try:
+#    angka = int(input("Masukkan angka: "))
+# except ValueError:
+#    print("Input yang anda masukkan bukan angka!")
+# else:
+#    print(f"Angka yang kamu input: {angka}")
+
+
+# try:
+#    angka = int(input("Masukkan angka: "))
+# except ValueError:
+#    print("Input yang anda masukkan bukan angka")
+# else:
+#    print(f"Angka yang kamu input: {angka}")
+# finally:
+#    print("Program selesai")
+
+
+# try:
+#    nama = input("Masukkan nama").strip()
+#    if not nama.strip():
+#        raise ValueError("Nama tidak boleh lebih dari 5 karakter")
+# except ValueError as e:
+#    print(e)
+
+# import json
+
+# def coba():
+#     while True:
+#         try:
+#                angka = int(input("Masukan angka: "))
+#                print(angka)
+#                break
+#         except:
+#             print("bukan angka")
+
+# coba()
+
+# file = open('data.txt','r')
+# print(file)
+
+# with open('data.txt','r') as file:
+#     konten = file.read()
+#     print(konten)
+
+# file = open('example.txt', 'w')
+
+# path = 'data.json'
+
+# def readdata():
+#    with open(path) as json_file:
+#        # return json_file
+#        return json.load(json_file)
+
+# def bacadata(data_baru):
+#    with open('path','w')
+
+
+
+
+
+
+
